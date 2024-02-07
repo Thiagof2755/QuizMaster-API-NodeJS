ea0821b5ff186251fc05528d7d7a700f363b4ab3
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2024-02-07 15:33:14 -0300

    Api ok

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,18 +1,34 @@
 import Question from "../models/Question.js";
 
 class QuestionController {
-    static getAllQuestions = async (_, res) => {
+    static async getAllQuestions(req, res) {
+        const apiKey = req.query.apiKey;
+
+        // Verifique se a chave de autenticação é válida
+        if (apiKey !== process.env.API_KEY) {
+            return res.status(401).json({ message: "Chave de autenticação inválida." });
+        }
+
+        // Se a chave for válida, continue com a lógica da função
         try {
             const questions = await Question.find();
             res.json(questions);
         } catch (error) {
             res.status(500).json({ message: "Erro ao buscar perguntas." });
         }
-    };    
-    static async createQuestion(req, res) {
-        try {
-            const novaPergunta = await Question.create(req.body);;
+    }
 
+    static async createQuestion(req, res) {
+        const apiKey = req.query.apiKey;
+
+        // Verifique se a chave de autenticação é válida
+        if (apiKey !== process.env.API_KEY) {
+            return res.status(401).json({ message: "Chave de autenticação inválida." });
+        }
+
+        // Se a chave for válida, continue com a lógica da função
+        try {
+            const novaPergunta = await Question.create(req.body);
             res.status(201).json({ message: "Pergunta criada com sucesso", novaPergunta });
         } catch (error) {
             res.status(500).json({ message: `${error.message} - falha na requisição` });
@@ -23,20 +39,34 @@ class QuestionController {
         const limit = parseInt(req.query.limit) || 10; 
         try {
             const questions = await Question.aggregate([{ $sample: { size: limit } }]);
-    
             const perguntas = questions.map((question) => {
                 const { resposta_correta, ...pergunta } = question;
                 return pergunta;
             });
-            
-            // Enviar as perguntas para o cliente
             res.json(perguntas);
         } catch (error) {
-            // Lidar com erros
             res.status(500).json({ message: "Erro ao buscar perguntas." });
         }
-    }    
-}    
+    }
     
+
+    static async ValidateAnswer(req, res) {
+        const { id, resposta } = req.body;
+        try {
+            const question = await Question.findById(id);
+            if (question) {
+                if (question.resposta_correta === resposta) {
+                    res.status(200).json({ message: "Resposta correta!" });
+                } else {
+                    res.status(400).json({ message: "Resposta incorreta!" });
+                }
+            } else {
+                res.status(404).json({ message: "Pergunta não encontrada." });
+            }
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao validar resposta." });
+        }
+    }   
+}
 
 export default QuestionController;
--- src/routes/QuestionRouters.js
+++ src/routes/QuestionRouters.js
@@ -1,5 +1,4 @@
 import express from "express";
-
 import QuestionController from "../controllers/QuestionController.js";
 
 const routes = express.Router();
@@ -7,6 +6,7 @@ const routes = express.Router();
 routes.get("/AllQuestion", QuestionController.getAllQuestions);
 routes.post("/CreateQuestion", QuestionController.createQuestion);
 routes.get("/Question", QuestionController.getQuestion);
+routes.post("/ValidateAnswer", QuestionController.ValidateAnswer);
 
 export default routes;
 
