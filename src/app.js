13b9eddcbeb7241057217901329f04c8902f877a
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2024-02-09 17:54:20 -0300

    Cors update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
+import cors from "cors"; // Importe o módulo cors
 import routes from "./routes/index.js";
-
 
 const conexao = await conectaNaDatabase();
 
 const app = express();
+
+// Use o middleware cors
+app.use(cors());
+
 routes(app);
+
 conexao.on("error", (erro) => {
   console.error("erro de conexão", erro);
 });
@@ -14,6 +19,5 @@ conexao.on("error", (erro) => {
 conexao.once("open", () => {
     console.log("Conexão com o banco feita com sucesso");
 })
-
 
 export default app;
